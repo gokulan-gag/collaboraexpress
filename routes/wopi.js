7048e7db97d9c3b77fb56724ea47fcd7feb12b17
Gokulan <gokulan@gridsandguides.com>
2023-07-14 13:16:21 +0530

    _

--- routes/wopi.js
+++ routes/wopi.js
@@ -9,75 +9,91 @@ const s3 = new AWS.S3();
 
 /* *
  *  wopi CheckFileInfo endpoint
- *
- *  Returns info about the file with the given document id.
- *  The response has to be in JSON format and at a minimum it needs to include
- *  the file name and the file size.
- *  The CheckFileInfo wopi endpoint is triggered by a GET request at
- *  https://HOSTNAME/wopi/files/<document_id>
  */
+
 router.get("/files/:fileId", function (req, res) {
   console.log("file id: " + req.params.fileId);
-  // test.txt is just a fake text file
-  // the Size property is the length of the string
-  // returned by the wopi GetFile endpoint
+  let params = { Bucket: "demo-s3-bucket-july", Key: "" };
 
-  s3.headObject(
-    { Bucket: "demo-s3-bucket-july", Key: "sampledocx.docx" },
-    (err, data) => {
-      if (err) {
-        console.log("Error retrieving file information:", err);
-        res.sendStatus(404);
-      } else {
-        console.log(data);
-        const fileInfo = {
-          BaseFileName: "sampledocx.docx",
-          Size: data.ContentLength,
-          // Include any additional properties you need
-          UserId: 1,
-          UserCanWrite: true,
-        };
+  const fileID = req.params.fileId;
 
-        res.json(fileInfo);
-      }
+  if (fileID == "1") {
+    params.Key = "sampledocx.docx";
+  }
+
+  if (fileID == "2") {
+    params.Key = "samplePPTX.pptx";
+  }
+
+  if (fileID == "3") {
+    params.Key = "sampleXLSX.xlsx";
+  }
+
+  if (fileID == "4") {
+    params.Key = "samplePDF.pdf";
+  }
+
+  if (fileID == "5") {
+    params.Key = "sampleJPG.jpg";
+  }
+
+  s3.headObject(params, (err, data) => {
+    if (err) {
+      console.log("Error retrieving file information:", err);
+      res.sendStatus(404);
+    } else {
+      console.log(data);
+      const fileInfo = {
+        BaseFileName: "sampledocx.docx",
+        Size: data.ContentLength,
+        UserId: 1,
+        UserCanWrite: true,
+      };
+
+      res.json(fileInfo);
     }
-  );
-  // res.json({
-  //   BaseFileName: "test.pptx",
-  //   Size: 11,
-  //   UserId: 1,
-  //   UserCanWrite: true,
-  // });
+  });
 });
 
 /* *
  *  wopi GetFile endpoint
- *
- *  Given a request access token and a document id, sends back the contents of the file.
- *  The GetFile wopi endpoint is triggered by a request with a GET verb at
- *  https://HOSTNAME/wopi/files/<document_id>/contents
  */
 router.get("/files/:fileId/contents", function (req, res) {
-  // we just return the content of a fake text file
-  // in a real case you should use the file id
-  // for retrieving the file from the storage and
-  // send back the file content as response
-  // s3.getObject(
-  //   { Bucket: "demo-s3-bucket-july", Key: "sampledocx.docx" },
-  //   function (err, data) {
-  //     if (err) {
-  //       console.log("Error retrieving file content:", err);
-  //       res.sendStatus(404);
-  //     } else {
-  //       const fileContent = data.Body;
-  //       console.log("DATA....", data);
-  //       res.send(fileContent);
-  //     }
-  //   }
-  // );
+  console.log("file id: " + req.params.fileId);
+  let params = { Bucket: "demo-s3-bucket-july", Key: "" };
 
-  var fileContent = "Hello world!";
-  res.send(fileContent);
+  const fileID = req.params.fileId;
+
+  if (fileID == "1") {
+    params.Key = "sampledocx.docx";
+  }
+
+  if (fileID == "2") {
+    params.Key = "samplePPTX.pptx";
+  }
+
+  if (fileID == "3") {
+    params.Key = "sampleXLSX.xlsx";
+  }
+
+  if (fileID == "4") {
+    params.Key = "samplePDF.pdf";
+  }
+
+  if (fileID == "5") {
+    params.Key = "sampleJPG.jpg";
+  }
+
+  s3.getObject(params, function (err, data) {
+    if (err) {
+      console.log("Error retrieving file content:", err);
+      return res.sendStatus(404);
+    } else {
+      const fileContent = data.Body;
+      console.log("DATA....", data);
+      return res.send(fileContent);
+    }
+  });
 });
 
 /* *
@@ -92,8 +108,8 @@ router.post("/files/:fileId/contents", function (req, res) {
   // to check that saving has triggered this wopi endpoint
   console.log("wopi PutFile endpoint");
   if (req.body) {
-    console.dir(req.body);
-    console.log(req.body.toString());
+    console.dir("SAVED DATA....", req.body);
+    // console.log(req.body.toString());
     res.sendStatus(200);
   } else {
     console.log("Not possible to get the file content.");
